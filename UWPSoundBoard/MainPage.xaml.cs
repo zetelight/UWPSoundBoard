524edade4e5880bb679b2c694b0325879906bca1
Xin Chen <25371490+zetelight@users.noreply.github.com>
2017-09-10 13:06:57 -0700

    Update  for completing project

--- UWPSoundBoard/MainPage.xaml.cs
+++ UWPSoundBoard/MainPage.xaml.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UWPSoundBoard.Model;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +27,128 @@ namespace UWPSoundBoard
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<Sound> Sounds;
+        private List<MenuItem> MenuItems;
+        private List<String> Suggestions;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            Sounds = new ObservableCollection<Sound>();
+            SoundManager.GetAllSounds(Sounds);
+
+            MenuItems = new List<MenuItem>();
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/animals.png", Category = SoundCategory.Animals});
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/cartoon.png", Category = SoundCategory.Cartoons });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/taunt.png", Category = SoundCategory.Taunts });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/warning.png", Category = SoundCategory.Warnings });
+
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            goBack();
+        }
+
+        public void goBack()
+        {
+            SoundManager.GetAllSounds(Sounds);
+            MenuItemsListView.SelectedItem = null;
+            CategoryTextBlock.Text = "All Sounds";
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void MenuItemsListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = (MenuItem)e.ClickedItem;
+            CategoryTextBlock.Text = menuItem.Category.ToString();
+            SoundManager.GetAllSoundsByCategory(Sounds, menuItem.Category);
+            BackButton.Visibility = Visibility.Visible;
+        }
+
+        private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var sound = (Sound)e.ClickedItem;
+            MyMediaElement.Source = new Uri(this.BaseUri, sound.AudioFile);
+        }
+
+        private async void SoundGridView_Drop(object sender, DragEventArgs e)
+        {
+            // first we check the items we dragged actually are files from Storage
+            // which means they are neither some texts nor something from other apps
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync(); //get all items reference
+                // check whether there's something in items
+                if (items.Any())
+                {
+                    // get the first item which means the index is 0
+                    var storageFile = items[0] as StorageFile;
+
+                    // get type of item
+                    var contentType = storageFile.ContentType;
+
+                    // get reference for local folder where we store the data for this app
+                    StorageFolder folder = ApplicationData.Current.LocalFolder;
+                    // after the step above, we can get path of the folder
+
+                    if (contentType == "audio/wav" || contentType == "audio/mpeg")
+                    {
+                        StorageFile newFile = await storageFile.CopyAsync(folder, storageFile.Name, NameCollisionOption.GenerateUniqueName);
+                        //Maker is something pop up at an exact moment we set, give some information
+                        //like Youtube's ads.
+                        /* Creating a marker ... could come in handy.
+                        MediaPlayer.Markers.Clear();
+                        MediaPlayer.MarkerReached += MediaPlayer_MarkerReached;
+                        var marker = new TimelineMarker();
+                        marker.Time = new TimeSpan(0, 0, 3);
+                        MediaPlayer.Markers.Add(marker);
+                        */
+                        MyMediaElement.SetSource(await storageFile.OpenAsync(FileAccessMode.Read), contentType);
+                        //MediaPlayerStoryboard.Begin();
+                        //MediaPlayer.Source = new Uri(somesuch, UriKind.Absolute);
+                        MyMediaElement.Play();
+                    }
+                }
+            }
+        }
+
+        private void SoundGridView_DragOver(object sender, DragEventArgs e)
+        {
+            // using Windows.ApplicationModel.DataTransfer;
+            // let the event accepted Operation be a DataPackageOperation.Copy so that it can be copied in our app
+            e.AcceptedOperation = DataPackageOperation.Copy;
+
+            // Drag adorner ... change what mouse / icon looks like
+            // as you're dragging the file into the app:
+            // http://igrali.com/2015/05/15/drag-and-drop-photos-into-windows-10-universal-apps/
+            e.DragUIOverride.Caption = "drop to create a custom sound and tile";
+            e.DragUIOverride.IsCaptionVisible = true;
+            e.DragUIOverride.IsContentVisible = true; //the content of the file itself
+            e.DragUIOverride.IsGlyphVisible = true; //for icons in the Caption
+        }
+        
+        private void SearchAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (String.IsNullOrEmpty(sender.Text)) goBack();
+
+            SoundManager.GetAllSounds(Sounds);
+            Suggestions = Sounds.Where(p => p.Name.StartsWith(sender.Text)).Select(p => p.Name).ToList();
+            SearchAutoSuggestBox.ItemsSource = Suggestions;
+        }
+
+        private void SearchAutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            SoundManager.GetAllSoundsByName(Sounds, sender.Text);
+            MenuItemsListView.SelectedItem = null;
+            CategoryTextBlock.Text = sender.Text;
+            BackButton.Visibility = Visibility.Visible;
         }
     }
 }
---
+++ UWPSoundBoard/Model/SoundManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBoard.Model
+{
+    public class SoundManager
+    {
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSounds = getSounds();
+            sounds.Clear(); //clear all before we add
+            allSounds.ForEach(p => sounds.Add(p)); //add all sounds into sounds
+            
+        }
+
+        public static void GetAllSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory soundcategory)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(p => p.Category == soundcategory).ToList();
+            //both ways same
+            //allSounds.Where(p => p.Category == soundcategory).ToList();
+            //(from sound in allSounds
+            //where sound.Category == soundcategory
+            //select sound).ToList();
+
+            sounds.Clear(); //clear all before we add
+            filteredSounds.ForEach(p => sounds.Add(p)); //add all sounds into sounds
+
+        }
+
+
+        private static List<Sound> getSounds()
+        {
+            var sounds = new List<Sound>();
+
+            sounds.Add(new Sound("Cow", SoundCategory.Animals));
+            sounds.Add(new Sound("Cat", SoundCategory.Animals));
+
+            sounds.Add(new Sound("Gun", SoundCategory.Cartoons));
+            sounds.Add(new Sound("Spring", SoundCategory.Cartoons));
+
+            sounds.Add(new Sound("Clock", SoundCategory.Taunts));
+            sounds.Add(new Sound("LOL", SoundCategory.Taunts));
+
+            sounds.Add(new Sound("Ship", SoundCategory.Warnings));
+            sounds.Add(new Sound("Siren", SoundCategory.Warnings));
+
+            return sounds;
+        }
+
+        public static void GetAllSoundsByName(ObservableCollection<Sound> sounds, string name)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(p => p.Name == name).ToList();
+            sounds.Clear(); //clear all before we add
+            filteredSounds.ForEach(p => sounds.Add(p)); //add all sounds into sounds
+
+        }
+    }
+}
